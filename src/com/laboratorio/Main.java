0948ca00d7f8549964792c14181c6efeffc308cf
Jorge Mario <jmla196a@gmail.com>
2025-03-29 21:38:09 -0500

    Prueba conflicto, commit Main.java

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -2,6 +2,9 @@ package com.laboratorio;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos al laboratorio de Git con Java");
+		System.out.println("Bienvenidos al laboratorio de Git con Java, prueba conflicto");
+	}
+	public static void conflict(String[] args) {
+		System.out.println("Prueba conflicto");
 	}
 }
