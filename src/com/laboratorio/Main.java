7eec99057e172ef4c91d0a88084ba3f05e3ce60c
Your Name <you@example.com>
2025-03-29 22:30:22 -0500

    Añadir Prueba_conflicto_1

--- src/com/laboratorio/Main.java
+++ src/com/laboratorio/Main.java
@@ -2,7 +2,9 @@ package com.laboratorio;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Bienvenidos al laboratorio de Git con Java");
+		System.out.println("Bienvenidos al laboratorio de Git con Java puebra 1");
+		System.out.println("Prueba 2");
+		System.out.println("Prueba 3");
 	}
 	public int sumar(int a, int b) {
 	    return a + b;
