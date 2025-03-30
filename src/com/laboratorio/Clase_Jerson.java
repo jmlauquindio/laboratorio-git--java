2b0621cf3010bc1b6f0ef0986d500263a5692985
Jorge Mario <jmla196a@gmail.com>
2025-03-29 21:35:00 -0500

    Prueba conflicto, commit.

--- src/com/laboratorio/Clase_Jerson.java
+++ src/com/laboratorio/Clase_Jerson.java
@@ -3,6 +3,8 @@ package com.laboratorio;
 public class Clase_Jerson {
 
 	public static void main(String[] args) {
-		System.out.println("Mensaje desde constructor de la clase");
+		System.out.println("Mensaje desde constructor de la clase, modificado para conflicto.");
+		System.out.println("Prueba conflicto, mensaje 2.");
+		System.out.println("Prueba conflicto, mensaje 3.");
 	}
 	}
